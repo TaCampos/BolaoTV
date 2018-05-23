ba15124da7c75e0fffbf0bf87f5fd458808bd15b
Tamara Martinelli de Campos <tamaramcampos02@gmail.com>
2018-05-23 10:03:03 -0300

    start add bet screen

---
+++ Bolao/Presentation/ViewController/AddBetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddBetViewController.swift
+//  Bolao
+//
+//  Created by Tamara Martinelli de Campos on 22/05/18.
+//  Copyright © 2018 Tamara Martinelli de Campos. All rights reserved.
+//
+
+import UIKit
+
+class AddBetViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var teamOneScore: UITextField!
+    @IBOutlet weak var teamTwoScore: UITextField!
+
+
+    @IBOutlet weak var viewTest: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+
+    }
+}
